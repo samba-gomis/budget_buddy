f1e1fe9f5b3175c98f699c11a045d65073f8f936
mahira-manico <mahira.manico@laplateforme.io>
2026-03-17 22:38:39 +0100

    feat: add of get_user model

--- budget_buddy/user_models.py
+++ budget_buddy/user_models.py
@@ -1,5 +1,39 @@
 import sqlite3
-from databases.database import init_db
+from databases.database import *
 
-init_db()
+def create_user(last_name, first_name, email, password):
+
+    conn=get_connection()
+    cursor=conn.cursor()
+
+    try:
+        request="INSERT INTO users (last_name, first_name, email, password) VALUES (?,?,?,?)"
+        values=(last_name, first_name, email, password)
+        cursor.execute(request, values)
+        conn.commit()
+
+    except sqlite3.IntegrityError:
+        print("Error, E-mail already used")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        conn.close()
+
+def get_user(email):
+
+    conn=get_connection()
+    cursor=conn.cursor()
+
+    try:
+        request="SELECT * from user WHERE email=?"
+        value=(email,)
+        cursor.execute(request,value)
+        return cursor.fetchall()
+    
+    finally:
+        conn.close()
+        
+
+  
+
 
